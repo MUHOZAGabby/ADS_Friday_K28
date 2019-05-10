65b9d168b718cff48a695a100346e9dbc9207c86
Szymon <Student@DESKTOP-6QVR8SB>
2019-05-10 10:16:33 +0200

    Simple UI

--- RPNCalculator/RPNCalculator/Program.cs
+++ RPNCalculator/RPNCalculator/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace RPNCalculator
 {
@@ -10,6 +7,17 @@ namespace RPNCalculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, this is RPN calculator");
+            Console.WriteLine("Expression: 2 3 + ");
+            string expression = "2 3 +";
+            double result = EvaluateRPNExpression(expression);
+            Console.WriteLine("Result: {0}", result);
+            Console.WriteLine("Expected result: 5");
+        }
+
+        static double EvaluateRPNExpression(string expr) {
+            double result = 0;
+            return result;
         }
     }
 }
